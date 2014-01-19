73cae5196761469d30b7e13afaa8fcfb214258d3
albertodotcom <alberto.forn@gmail.com>
2014-01-19 15:43:02 +1100

    w2d5 Create history for movies

--- students/alberto_forni/w2d4/main.rb
+++ students/alberto_forni/w2d4/main.rb
@@ -5,31 +5,83 @@ require 'active_support/all'
 require 'httparty'
 require 'uri'
 
-helpers do
-  def get_movie(movie_titile)
-    movie_name = URI.escape(movie_titile)
-    JSON.parse(HTTParty.get("http://www.omdbapi.com/?t=#{movie_name}"))
-  end
+before do
+  @visited_movies_file = 'visited_movies.json'
+  @movies_history = load_movies
 end
 
+helpers do
+  # get a single movie
+  def get_movie(movie_id)
+    
+    if @movies_history[movie_id] == nil
+      # insert the movie in the history
+      movie =  JSON.parse(HTTParty.get("http://www.omdbapi.com/?i=#{movie_id}"))
+      save_movie(movie)
+    else
+      # take the movie out of the list
+      movie = @movies_history[movie_id]
+    end
+
+    movie
+  end
+
+  # save movie history
+  def save_movie(movie)
+    @movies_history[movie["imdbID"]] = movie
+    File.open(@visited_movies_file, 'w') do |f|
+      f.write(JSON.generate(@movies_history))
+    end
+  end
+
+  # load movie history
+  def load_movies
+    json = File.read(@visited_movies_file)
+
+    if json.empty?
+      {}
+    else
+      JSON.parse(json)
+    end
+  end
+
+end
+
+# display list of movie or single movie or history
 get "/" do
   @movies = []
   
+  # form subscription
   if params[:title].present?
     movie_name = URI.escape(params[:title])
-    @movies = JSON.parse(HTTParty.get("http://www.omdbapi.com/?s=#{movie_name}"))["Search"]
+    @movies = JSON.parse(HTTParty.get("http://www.omdbapi.com/?s=#{movie_name}"))["Search"] || []
+
+    return haml :list
   end
 
+  # if there is just 1 movie or the users checked the "I'm feel lucky option" show the movie
   if @movies.size == 1 || params[:lucky] == "t"
-    @movie = get_movie(@movies[0]["Title"])
-    @movies = nil
+    @movie = get_movie(@movies[0]["imdbID"])
+
+    return haml :movie
   end
 
-  haml :index
+  haml :history
 end
 
-get "/movie/:movie_title" do
-  @movie = get_movie(params[:movie_title]) if params[:movie_title].present?
+# dispaly a movie
+get "/movie/:movie_id" do
+  redirect '/' unless params[:movie_id].present?
 
-  haml :index
+  movie_id = params[:movie_id]
+
+  @movie = get_movie(movie_id)
+
+  haml :movie
+end
+
+# show history
+get "/history" do
+
+  haml :history
 end
