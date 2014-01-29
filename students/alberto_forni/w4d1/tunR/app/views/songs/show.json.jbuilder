7a7df644a93707a540ab871b9f2c5a92fbef2574
albertodotcom <alberto.forn@gmail.com>
2014-01-29 17:58:48 +1100

    create app scaffold

---
+++ students/alberto_forni/w4d1/tunR/app/views/songs/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @song, :id, :name, :length, :description, :artist_id, :album_id, :created_at, :updated_at
---
+++ students/alberto_forni/w4d1/tunR/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TunR::Application.config.secret_key_base = 'b18c7db1cdf796221a3e01811da175f8f1899678b68f87e1f2c3016eb44683c4b578c697a1d1aa1ad3a7c4f7ea3ee3205111e66beed81eac71842046e8e49267'
